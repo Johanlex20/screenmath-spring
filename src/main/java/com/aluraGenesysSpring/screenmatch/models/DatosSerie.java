095d224e3141c9b58a194a8a0a9f49c2a1c057ff
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-06-04 10:03:27 -0500

    - importando jsondatabin a denpendencias - crear clase DatosSerie y poner anotaciones para ver los datos del api y convertirlos datos java

---
+++ src/main/java/com/aluraGenesysSpring/screenmatch/models/DatosSerie.java
@@ -0,0 +1,10 @@
+package com.aluraGenesysSpring.screenmatch.models;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record DatosSerie(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons") Integer totalDeTemporadas,
+        @JsonAlias("imdbRating") String evaluacion
+) {
+}
