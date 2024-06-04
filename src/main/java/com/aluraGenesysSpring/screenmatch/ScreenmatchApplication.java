6b0183834860149de1408a7a168fc8656047011f
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-06-04 11:04:25 -0500

    Creando clase principal y refactorizando el codigo para que sea llamado desde la clase principal y no desde la de spring pero instanciar la Principal para mostrar el menu

--- src/main/java/com/aluraGenesysSpring/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluraGenesysSpring/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package com.aluraGenesysSpring.screenmatch;
 import com.aluraGenesysSpring.screenmatch.models.DatosEpisodio;
 import com.aluraGenesysSpring.screenmatch.models.DatosSerie;
 import com.aluraGenesysSpring.screenmatch.models.DatosTemporadas;
+import com.aluraGenesysSpring.screenmatch.principal.Principal;
 import com.aluraGenesysSpring.screenmatch.services.ConsumoApi;
 import com.aluraGenesysSpring.screenmatch.services.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
@@ -21,27 +22,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		ConsumoApi consumoApi = new ConsumoApi();
-		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=2de77f45");
-		System.out.println(json);
-
-		ConvierteDatos conversor = new ConvierteDatos();
-		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datos);
-
-		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=y&apikey=2de77f45");
-		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
-		System.out.println(episodios);
-
-		//CREAR LISTA PARA VER LAS TEMPORADASA
-		List<DatosTemporadas> temporadas = new ArrayList<>();
-		for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
-			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season="+i+"&apikey=2de77f45");
-			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
-			temporadas.add(datosTemporadas);
-		}
-		// imprimir todo lo que tenga ese lista
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.muestraElMenu();
 	}
 }
---
+++ src/main/java/com/aluraGenesysSpring/screenmatch/principal/Principal.java
@@ -0,0 +1,37 @@
+package com.aluraGenesysSpring.screenmatch.principal;
+
+import com.aluraGenesysSpring.screenmatch.models.DatosSerie;
+import com.aluraGenesysSpring.screenmatch.models.DatosTemporadas;
+import com.aluraGenesysSpring.screenmatch.services.ConsumoApi;
+import com.aluraGenesysSpring.screenmatch.services.ConvierteDatos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner sc = new Scanner(System.in);
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private static String URL_BASE ="https://www.omdbapi.com/?t=";
+    private static String API_KEY = "&apikey=2de77f45";
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraElMenu(){
+        System.out.println("Por favor escribe el nombre de la serie que deseas buscar:");
+        var nombreSerie= sc.nextLine();
+        var json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ","+") + API_KEY);
+        var datos = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datos);
+
+        //CREAR LISTA PARA VER LAS TEMPORADASA
+        List<DatosTemporadas> temporadas = new ArrayList<>();
+        for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
+            json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ","+") + "&Season=" +i+ API_KEY);
+            var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+            temporadas.add(datosTemporadas);
+        }
+        // imprimir todo lo que tenga ese lista
+        temporadas.forEach(System.out::println);
+    }
+
+}
